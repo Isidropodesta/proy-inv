68b814b07568abaeb2f7a98173ea7220f82c1bfe
vincenzo <dallapevichen12@gmail.com>
2025-05-21 23:43:44 -0300

    Agrego componentes Header y Footer, modifico App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import React from "react";
+import Header from "./assets/components/Header/header";
+import Footer from "./assets/components/Footer/footer";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Header />
+      {/* Aquí va tu contenido principal */}
+      <main className="container mx-auto p-6">
+        <h2 className="text-xl font-semibold">Bienvenido a StockPro</h2>
+      </main>
+      <Footer />
+    </>
+  );
 }
