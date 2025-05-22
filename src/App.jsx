e8f37967ff976158d231f5b61f79832d4165ae7a
vincenzo <dallapevichen12@gmail.com>
2025-05-21 23:54:35 -0300

    fix problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
-=======
 import React from "react";
 import Header from "./assets/components/Header/header";
 import Footer from "./assets/components/Footer/footer";
@@ -23,4 +14,4 @@ export default function App() {
     </>
   );
 }
->>>>>>> front-vichen
+
