3361c4a0b0ca635d01ef67f417c20c566b8d27da
vincenzo dallape <dallapevichen12@gmail.com>
2025-05-30 01:22:16 -0300

    conexion con repo back lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./assets/components/Header/header.jsx";
 import Hero from "./assets/components/Hero/hero.jsx";
 import Footer from "./assets/components/Footer/footer.jsx";
@@ -6,12 +6,19 @@ import OrdenDeCompra from "./assets/components/Orden-compra/orden-compra.jsx";
 import Ventas from "./assets/components/Ventas/ventas.jsx";
 
 function App() {
+useEffect(() => {
+  fetch(`${import.meta.env.VITE_API_URL}/saludo`)  // Ojo: /saludo y no /test
+    .then((res) => res.json())
+    .then((data) => console.log("Respuesta del backend:", data))
+    .catch((err) => console.error("Error al conectar con el backend:", err));
+}, []);
+
   return (
     <div>
       <Header />
       <Hero />
-      <OrdenDeCompra/>
-      <Ventas/>
+      <OrdenDeCompra />
+      <Ventas />
       <Footer />
     </div>
   );
